3be8622dfc99f39591a521aaad285a83bbbbbd1a
Tu's VR machine Win10x64 <tdinh7@villanova.edu>
2017-09-04 18:41:20 -0400

    this is the first commit and this is a commit message

---
+++ index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WordJumble.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Flamingos Rock! Yeah!
+        </div>
+
+        <br />
+        <div>
+            <asp:Button ID="btnHello" runat="server" Text="Why did Adele cross the road?" OnClick="btnHello_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WordJumble
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHello_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(),"Scripts","<script>alert('To say hello from the other side! :-) ');</script>");
+        }
+    }
+}
